dcdbd758a42f48634a24cbd838c95adcc3871b17
gunwoda <95297035+gunwoda@users.noreply.github.com>
2022-06-21 22:28:30 +0900

    Changes

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,4 +1,4 @@
-"""Final URL Configuration
+"""djangoProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
